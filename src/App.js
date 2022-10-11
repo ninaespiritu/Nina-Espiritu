6dc0151e61552430b20e4c78abf35b6d55b204e5
Nina Espiritu <ninaangela.espiritu@gmail.com>
2022-10-11 06:12:26 +0100

    Add style to Intro and HeroImage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import Intro from './components/Intro';
-import HeroImage from './components/HeroImage';
-import './App.css';
+import Intro from "./components/Intro";
+import HeroImage from "./components/HeroImage";
+import "./App.css";
 
 const App = () => {
-  return (
-    <div>
-      <Intro />
-      <HeroImage />
-    </div>
-  );
-}
+	return (
+		<div className="app">
+			<div className="app-intro">
+				<Intro />
+			</div>
+			<div className="app-all">
+				<HeroImage />
+			</div>
+		</div>
+	);
+};
 
 export default App;
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -2,16 +2,18 @@ import "../styles/Intro.css";
 
 const Intro = () => {
 	return (
-		<div id="intro" className="intro">
-			<h1>Hello, I'm Nina</h1>
-			<h2>Developer + Designer</h2>
-			<p>
-				I am a passionate tech professional with a keen interest in
-				full-stack development, graphic design, user exprience, and
-				everything in between.
-			</p>
-			<button>Contact Me</button>
-		</div>
+		<header className="intro">
+			<div id="intro" className="intro-info">
+				<h1>Hello, I'm Nina</h1>
+				<h2>Developer + Designer</h2>
+				<p>
+					I am a passionate tech professional with a keen interest in
+					full-stack development, graphic design, user exprience, and
+					everything in between.
+				</p>
+				<button>Contact Me</button>
+			</div>
+		</header>
 	);
 };
 
