96c56f11c2b7793c9b06c5d6e8e70a9bb6b3b162
Nina Espiritu <ninaangela.espiritu@gmail.com>
2022-10-11 03:50:55 +0100

    Add HeroImage component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
 import Intro from './components/Intro';
+import HeroImage from './components/HeroImage';
+import './App.css';
 
 const App = () => {
   return (
     <div>
       <Intro />
+      <HeroImage />
     </div>
   );
 }
---
+++ src/components/HeroImage.js
@@ -0,0 +1,15 @@
+import "../styles/HeroImage.css";
+import HeroImg from "../images/HeroImage.png";
+
+const HeroImage = () => {
+	return (
+		<div>
+			<img
+				src={HeroImg}
+				alt="Digital illustration of a woman working at a desk"
+			/>
+		</div>
+	);
+};
+
+export default HeroImage;
