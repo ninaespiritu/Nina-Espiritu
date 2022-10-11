e8a528efb3e531b05ae4fb2d1d61934ccdba6164
Nina Espiritu <ninaangela.espiritu@gmail.com>
2022-10-11 03:29:02 +0100

    Add Intro component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Intro from './components/Intro';
 
 const App = () => {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Intro />
     </div>
   );
 }
---
+++ src/components/Intro.js
@@ -0,0 +1,18 @@
+import "../styles/Intro.css";
+
+const Intro = () => {
+	return (
+		<div>
+			<h1>Hello, I'm Nina</h1>
+			<h2>Developer + Designer</h2>
+			<p>
+				I am a passionate tech professional with a keen interest in
+				full-stack development, graphic design, user exprience, and
+				everything in between.
+			</p>
+			<button>Contact Me</button>
+		</div>
+	);
+};
+
+export default Intro;
