bb747b888ba75bd54c08677a089f30a4981bd1f7
Nina Espiritu <ninaangela.espiritu@gmail.com>
2022-10-18 21:27:37 +0100

    Add Contact component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HeroImage from "./components/HeroImage";
 import About from "./components/About";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 const App = () => {
 	return (
@@ -16,6 +17,7 @@ const App = () => {
 				<About />
 				<Experience />
 				<Projects />
+				<Contact />
 			</div>
 		</div>
 	);
---
+++ src/components/Contact.js
@@ -0,0 +1,11 @@
+import "../styles/Contact.css";
+
+const Contact = () => {
+	return (
+		<div>
+			<h2>Get In Touch</h2>
+		</div>
+	);
+};
+
+export default Contact;
