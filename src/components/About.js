9f4e65ce6f98ef7a38c0f53e85996bcd00d4b439
Nina Espiritu <ninaangela.espiritu@gmail.com>
2022-10-19 00:51:20 +0100

    Install react-icons & update existing icons

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,7 @@
 import "../styles/About.css";
+import { HiArrowRight } from "react-icons/hi";
 
 const About = () => {
-	const arrow = (
-		<svg width="16" height="16" viewBox="0 0 16 16">
-			<path d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
-		</svg>
-	);
-
 	return (
 		<div className="about">
 			<h2>About Me</h2>
@@ -27,22 +22,22 @@ const About = () => {
 			<p>Here are some technologies I have been working with:</p>
 			<ul className="about-skills">
 				<li>
-					<span>{arrow}</span> JavaScript E6+
+					<span><HiArrowRight /></span> JavaScript E6+
 				</li>
 				<li>
-					<span>{arrow}</span> HTML &amp; CSS
+					<span><HiArrowRight /></span> HTML &amp; CSS
 				</li>
 				<li>
-					<span>{arrow}</span> React.js
+					<span><HiArrowRight /></span> React.js
 				</li>
 				<li>
-					<span>{arrow}</span> Node.js
+					<span><HiArrowRight /></span> Node.js
 				</li>
 				<li>
-					<span>{arrow}</span> MongoDB
+					<span><HiArrowRight /></span> MongoDB
 				</li>
 				<li>
-					<span>{arrow}</span> Python
+					<span><HiArrowRight /></span> Python
 				</li>
 			</ul>
 		</div>
