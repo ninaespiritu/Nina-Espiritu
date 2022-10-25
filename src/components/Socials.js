86c1e5abc08cd25b7cafbd0d2931d7c3d3b3e4e9
Nina Espiritu <ninaangela.espiritu@gmail.com>
2022-10-25 05:42:05 +0100

    Create animations for Socials icons

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,46 +1,79 @@
 import "../styles/Socials.css";
 import { BsBehance, BsDribbble, BsGithub, BsLinkedin } from "react-icons/bs";
+import { motion } from "framer-motion";
 
 const Socials = () => {
+	const containerSocials = {
+		hidden: {
+			opacity: 0,
+		},
+		show: {
+			opacity: 1,
+			transition: {
+				staggerChildren: 0.15,
+				delayChildren: 2,
+			},
+		},
+	};
+
+	const itemSocials = {
+		hidden: {
+			opacity: 0,
+			y: -15,
+		},
+		show: {
+			opacity: 1,
+			y: 0,
+			transition: {
+				duration: 0.2,
+			},
+		},
+	};
+
 	return (
-		<div className="socials">
+		<motion.div
+			className="socials"
+			variants={containerSocials}
+			initial="hidden"
+			animate="show"
+		>
 			<a
 				href="https://github.com/ninaespiritu"
 				target="_blank"
 				rel="noopener noreferrer"
 			>
-				<div>
+				<motion.div variants={itemSocials}>
 					<BsGithub size="1.5em" />
-				</div>
+				</motion.div>
 			</a>
 			<a
 				href="https://www.behance.net/ninaespiritu"
 				target="_blank"
 				rel="noopener noreferrer"
 			>
-				<div>
+				<motion.div variants={itemSocials}>
 					<BsBehance size="1.5em" />
-				</div>
+				</motion.div>
 			</a>
 			<a
 				href="https://dribbble.com/ninaespiritu"
 				target="_blank"
 				rel="noopener noreferrer"
 			>
-				<div>
+				<motion.div variants={itemSocials}>
 					<BsDribbble size="1.5em" />
-				</div>
+				</motion.div>
 			</a>
 			<a
 				href="https://www.linkedin.com/in/nina-espiritu/"
 				target="_blank"
 				rel="noopener noreferrer"
 			>
-				<div>
+				<motion.div variants={itemSocials}>
 					<BsLinkedin size="1.5em" />
-				</div>
+				</motion.div>
 			</a>
-		</div>
+		</motion.div>
 	);
 };
 
